02ea34012ee8723fe65a0462cfb40bf49fc04631
imsuzuki <hello@imsuzuki.com>
2026-05-02 22:58:52 +0900

    Tweak LunarVim defaults

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -3,3 +3,14 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- vim-illuminate's treesitter provider currently trips Neovim 0.12's API.
+lvim.builtin.illuminate.options.providers = {
+  "lsp",
+  "regex",
+}
+
+-- Avoid Mason prompting to install Ruby's solargraph automatically.
+lvim.lsp.installer.setup.automatic_installation.exclude = {
+  "solargraph",
+}
